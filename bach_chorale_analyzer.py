2160dc508072f8326d0ea8a93d81d012d6804ffc
Stephanie Orlando <stephaniejulieorlando@gmail.com>
2020-08-02 16:32:51 -0400

    minor changes for RN setting

--- bach_chorale_analyzer.py
+++ bach_chorale_analyzer.py
@@ -1,17 +1,19 @@
 from music21 import *
 import csv
 
-chorale = corpus.parse('bwv43.11') # replace with chorale
-# chorale.remove(chorale.parts[0]) # remove any instrumental parts or comment out
-choraleKey = 'G' # replace with key
+chorale = corpus.parse('bach/bwv248.9-s.mxl') # replace with chorale
+#chorale.remove(chorale.parts[-1]) # remove any instrumental parts or comment out
+
+choraleKey = 'D' # replace with key
 
 choraleChords = chorale.chordify()
 
 choraleReduction = chorale.partsToVoices(voiceAllocation=2) 
 choraleReduction.parts[-1][0].clef = clef.BassClef()
 
-keySignature = key.Key(choraleKey)
-keySigAccidentals = set([str(a) for a in keySignature.alteredPitches] + [p.name for p in scale.MajorScale(choraleKey).pitches])
+
+keySig = key.Key(choraleKey)
+keySigAccidentals = set([p.name for p in scale.MajorScale(choraleKey).pitches] + [p.name for p in scale.MinorScale(choraleKey).pitches])
 
 measure = choraleReduction.parts[-1].recurse().getElementsByClass('Measure')
 chords = choraleChords.recurse().getElementsByClass('Chord')
@@ -33,10 +35,17 @@ def leadingTone(chord):
 		if s.accidental is not None:
 			if s.name not in keySigAccidentals:
 				return s
+			elif s.name == keySig.pitchFromDegree(7).transpose(-1).name and keySig.mode == 'major':
+				return s
 
 def modulatedKey(leadingNote):
-	modKey = key.Key(mode='major').deriveByDegree(7, leadingNote.name)
+	if leadingNote.name == keySig.pitchFromDegree(7).transpose(-1).name and keySig.mode == 'major':
+		modKey = key.Key(mode='major').deriveByDegree(4, leadingNote.name)
+	else:
+		modKey = key.Key(mode='major').deriveByDegree(7, leadingNote.name)
+
 	tonicChord = [p.name for p in modKey.pitchesFromScaleDegrees([1, 3, 5])]
+
 	if all(p in keySigAccidentals for p in tonicChord):
 		return modKey
 	else:
@@ -46,10 +55,10 @@ def harmonicAnalysis():
 	for c in chords:
 		if leadingTone(c) is not None:
 			rn = roman.romanNumeralFromChord(c, modulatedKey(leadingTone(c)))
-			rn_secondary = roman.romanNumeralFromChord(modulatedKey(leadingTone(c)).getChord(), keySignature)
+			rn_secondary = roman.romanNumeralFromChord(modulatedKey(leadingTone(c)).getChord(), keySig)
 			rnAnalysisList.append("%s/%s" % (rn.figure, rn_secondary.romanNumeral))
 		else:
-			rn = roman.romanNumeralFromChord(c, keySignature)
+			rn = roman.romanNumeralFromChord(c, keySig)
 			rnAnalysisList.append(rn.figure)
 	return rnAnalysisList
 	
@@ -120,6 +129,15 @@ def makeChoraleReduction():
 				rnAnalysisText.write("(%s) Beat: %s, Duration: %s\n" % (r, c.beatStr, c.quarterLength))
 
 		n += 1
+	"""
+		# if the chorale contains measures that begin with a rest, use this and comment text expression out
+		c.closedPosition(forceOctave=4, inPlace=True)
+		if f in fbDictFile and f != '2':
+			c.addLyric(r)
+		
+
+	choraleReduction.insert(0, choraleChords)
+	"""		
 
 	
 for m in measure:
@@ -128,7 +146,6 @@ for m in measure:
 
 for s in choraleReduction.parts:
 	s.finalBarline = 'final'
-
 
 makeChoraleReduction()
 choraleReduction.show()
